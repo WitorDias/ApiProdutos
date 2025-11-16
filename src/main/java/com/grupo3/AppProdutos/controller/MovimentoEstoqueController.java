887bfbbf75620e3415b29f3895bc1b1f401a42a2
WitorDias <joao_witor_dias@hotmail.com>
2025-11-16 10:00:59 -0300

    feat: Buscar lista de movimentos por produto adicionado ao MovimentoEstoqueController

--- src/main/java/com/grupo3/AppProdutos/controller/MovimentoEstoqueController.java
+++ src/main/java/com/grupo3/AppProdutos/controller/MovimentoEstoqueController.java
@@ -5,10 +5,9 @@ import com.grupo3.AppProdutos.model.MovimentoEstoque;
 import com.grupo3.AppProdutos.service.MovimentoEstoqueService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/movimentos")
@@ -33,6 +32,13 @@ public class MovimentoEstoqueController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(movimentoEstoqueService.registrarSaida(request.produtoId(), request.quantidade()));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<List<MovimentoEstoque>> buscarListaMovimentosPorProduto(@PathVariable Long id){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(movimentoEstoqueService.listarMovimentosPorProdutoId(id));
     }
 
 }
