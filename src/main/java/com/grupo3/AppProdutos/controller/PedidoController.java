b8f8d23e1856b13198d4a7344285158e0b7397b0
WitorDias <joao_witor_dias@hotmail.com>
2025-11-18 17:31:17 -0300

    feat: adicionar módulo de pedidos com criação e consulta por ID do pedido ou ID do usuário.

---
+++ src/main/java/com/grupo3/AppProdutos/controller/PedidoController.java
@@ -0,0 +1,44 @@
+package com.grupo3.AppProdutos.controller;
+
+import com.grupo3.AppProdutos.dto.PedidoRequest;
+import com.grupo3.AppProdutos.dto.PedidoResponse;
+import com.grupo3.AppProdutos.service.PedidoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/pedidos")
+public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PedidoResponse> criarPedido(@RequestBody PedidoRequest pedidoRequest){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(pedidoService.criarPedido(pedidoRequest));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidoResponse> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(pedidoService.buscarPedidoPorId(id));
+    }
+
+    @GetMapping("/usuario/{usuarioId}")
+    public ResponseEntity<List<PedidoResponse>> listarPorUsuario(@PathVariable Long usuarioId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(pedidoService.buscarPedidoPorUsuario(usuarioId));
+    }
+
+}
+
