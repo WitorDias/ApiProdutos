125a65a1853f61348043b9668c468f6ced6862ef
kau4dev <kauavictordev7@gmail.com>
2025-11-20 16:15:03 -0300

    feat: atualizar configurações de segurança e aprimorar documentação Swagger

--- src/main/java/com/grupo3/AppProdutos/config/security/SecurityConfig.java
+++ src/main/java/com/grupo3/AppProdutos/config/security/SecurityConfig.java
@@ -36,8 +36,14 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
                         // Swagger/OpenAPI
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        // Demais endpoints requerem autenticação
+                        .requestMatchers(
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class )
